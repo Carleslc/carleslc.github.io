36552c81a9e2aef8887af63cbb37e2755e7bb0b2
Carlos Lázaro Costa <lazaro.costa.carles@gmail.com>
2018-09-04 03:43:50 +0200

    Blog Feed + CV

--- js/index.js
+++ js/index.js
@@ -7,15 +7,6 @@ $(document).ready(function() {
   // COPYRIGHT
   $('#copyright-year').text(new Date().getFullYear());
 
-  // LOCAL LINKS
-  $('.page-link').click(function() {
-    var anchor = $(this).attr("dest");
-
-    $('html, body').animate({
-      scrollTop: $('#' + anchor).offset().top
-    }, 400);
-  });
-
   // STICKY NAVBAR
   var navbar = $('#navbar');
   var stickyHeight = navbar.offset().top;
@@ -27,6 +18,67 @@ $(document).ready(function() {
     } else {
       navbar.removeClass('sticky');
       $('#content').css("padding-top", 0);
+    }
+  });
+
+  // LOCAL LINKS
+  $('.page-link').click(function() {
+    var anchor = $(this).attr("dest");
+    var offset = 0;
+
+    if ($(this).attr("offset") != 'false') {
+      offset = navbar.height();
+    }
+
+    $('html, body').animate({
+      scrollTop: $('#' + anchor).offset().top - offset
+    }, 500);
+  });
+
+  // BLOG FEED
+  $("#rss-feeds").rss("https://blog.carleslc.me/feed.json",
+  {
+    limit: 4,
+    ssl: true,
+    host: '',
+    layoutTemplate: '<div class="feed-container container"><div class="row"><div class="col col-xs-12"><div class="blog-grids">{entries}</div></div></div></div>',
+    entryTemplate: '<div id="entry-{index}" class="feed-entry grid"><div class="entry-media">{entryImage}</div><div class="entry-body"><span class="cat">{category}</span><h3><a id="link-entry-{index}" href="{url}" target="_blank">{title}</a></h3><p>{shortBody}</p></div></div>',
+    effect: 'show', // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
+    tokens: {
+      entryImage: function(entry, tokens) {
+        if (tokens.banner) {
+          return `<img src="${tokens.banner}"/>`;
+        }
+        return '';
+      },
+      category: function(entry, tokens) {
+        return tokens.tags[0];
+      }
+    },
+    error: function() {
+      console.error('Cannot fetch blog feed');
+      console.warn(this);
+    },
+    success: function() {
+      var grids = $('.blog-grids');
+      $('div.feed-entry')
+      .click(function() {
+        window.open($(`#link-${$(this).attr('id')}`).attr('href'), '_blank');
+      })
+      .mouseenter(function() {
+        $(this).css('box-shadow', '0 0 30px rgba(0, 0, 0, 0.2)');
+        $(`#link-${$(this).attr('id')}`).toggleClass("hovered");
+        grids.children().not(this).fadeTo(0, 0.5);
+      })
+      .mouseleave(function() {
+        $(this).css('box-shadow', '0 0 30px rgba(0, 0, 0, 0.1)');
+        $(`#link-${$(this).attr('id')}`).toggleClass("hovered");
+        grids.children().fadeTo(0, 1);
+      });
+    },
+    onData: function() {
+      $('#blog-description').text(this.feed.description);
+      $('#blog-button').attr('href', this.feed.home_page_url);
     }
   });
 
