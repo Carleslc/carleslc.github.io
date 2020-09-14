b1acb3cacd6149a49954fc7e693010c074ac182a
Carleslc <lazaro.costa.carles@gmail.com>
2020-09-14 23:01:26 +0200

    Refactor RSS

--- js/index.js
+++ js/index.js
@@ -36,73 +36,19 @@ $(document).ready(function() {
   });
 
   // BLOG FEED
-  $("#rss-feeds").rss("https://blog.carleslc.me/feed.json",
-  {
-    limit: 4,
-    ssl: true,
-    host: '',
-    layoutTemplate: '<div class="grid center">{entries}</div>',
-    entryTemplate: '<div id="entry-{index}" class="feed-entry grid-element"><div class="entry-media">{entryImage}</div><div class="entry-body"><span class="cat">{category}</span><h3><p id="link-entry-{index}" href="{url}">{title}</p></h3><p>{shortBody}</p></div></div>',
-    effect: 'show', // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-    tokens: {
-      entryImage: function(_entry, tokens) {
-        if (tokens.banner) {
-          return `<img src="${tokens.banner}"/>`;
-        }
-        return '';
-      },
-      category: function(_entry, tokens) {
-        if (tokens.categories && tokens.categories.length > 0) {
-          return tokens.categories[0];
-        }
-      }
-    },
-    error: function() {
-      console.error('Cannot fetch blog feed');
-      console.warn(this);
-    },
-    success: function() {
-      var grid = $('.grid');
-      $('div.feed-entry')
-      .click(function() {
-        window.open($(`#link-${$(this).attr('id')}`).attr('href'), '_blank');
-      })
-      .mouseenter(function() {
-        $(this).css('box-shadow', '0 0 30px rgba(0, 0, 0, 0.2)');
-        $(`#link-${$(this).attr('id')}`).toggleClass("hovered");
-        grid.children().not(this).fadeTo(0, 0.5);
-      })
-      .mouseleave(function() {
-        $(this).css('box-shadow', '0 0 30px rgba(0, 0, 0, 0.1)');
-        $(`#link-${$(this).attr('id')}`).toggleClass("hovered");
-        grid.children().fadeTo(0, 1);
-      });
-    },
-    onData: function() {
-      $('#blog-description').text(this.feed.description);
-      $('#blog-button').attr('href', this.feed.home_page_url);
-    }
-  });
 
-  // CONTACT FORM with AJAX -- Gold account only :(
-  /*$('#contact-form').submit(function(e) {
-    $.ajax({
-      url: "https://formspree.io/lazaro.costa.carles@gmail.com",
-      method: "POST",
-      data: { message: $('#contact-form').serialize() },
-      dataType: "json"
-    }).done(function(response) {
-      $('#contact-error').addClass('hidden');
-      $('#contact-success').removeClass('hidden');
-      $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
-    }).fail(function(xhr, status, error) {
-      $('#contact-success').addClass('hidden');
-      $('#contact-error').removeClass('hidden');
-      console.warn(xhr.responseText);
-    }).always(function() {
-      $('.footer').height(600);
-      scrollToBottom();
+  $.getJSON("https://blog.carleslc.me/ghost/api/v3/content/posts/?key=c0a217e701348212fbbf260848&limit=4&order=published_at%20desc&include=tags", function callback(data) {
+    var posts = data.posts;
+    
+    var content = '<div class="grid center">';
+    posts.forEach(function(post) {
+      var image = `<img src="${post.feature_image}"/>`;
+      var tag = (post.primary_tag && post.primary_tag.name) || '';
+      content += `<a id="entry-${post.uuid}" class="feed-entry grid-element" href="${post.url}" target="_blank"><div class="entry-media">${image}</div><div class="entry-body"><span class="cat">${tag}</span><h3>${post.title}</h3><p>${post.excerpt}</p></div></a>`;
     });
-  });*/
+    content += '</div>';
+
+    $("#rss-feed").html(content);
+  });
 
 });
